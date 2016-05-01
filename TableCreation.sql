01fd8df9a094ed0f0a80b13e520f018e64a40c86
Michael Beaver <mbbh8@mst.edu>
2016-04-30 22:03:55 -0500

    Fixing VARCHAR Issue

--- TableCreation.sql
+++ TableCreation.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE Game (
   Name  VARCHAR(255) NOT NULL,
-  Console  VARCHAR() NOT NULL,
+  Console  VARCHAR(255) NOT NULL,
   PRIMARY KEY(Name)
 );
 
@@ -12,7 +12,7 @@ CREATE TABLE Event (
   Evenet_date DATE NOT NULL,
   Prize_money  INT,
   Max_participants  INT,
-  Location VARCHAR NOT NULL,
+  Location VARCHAR(255) NOT NULL,
   Start_time DATE NOT NULL,
   Is_streaming BOOLEAN NOT NULL,
   Provides_stream BOOLEAN NOT NULL,
@@ -21,9 +21,9 @@ CREATE TABLE Event (
 
 CREATE TABLE Player (
   Username VARCHAR(20),
-  F_name VARCHAR,
-  L_name VARCHAR,
-  Location VARCHAR,
+  F_name VARCHAR(255),
+  L_name VARCHAR(255),
+  Location VARCHAR(255),
   Ranking INT,
   PRIMARY KEY (Username)
 );
@@ -31,15 +31,15 @@ CREATE TABLE Player (
 CREATE TABLE Event_staff (
   Staff_id INT(9),
   Event_id INT(9)
-  F_name VARCHAR,
-  L_name VARCHAR,
+  F_name VARCHAR(255),
+  L_name VARCHAR(255),
   PRIMARY KEY(Staff_id),
   FOREIGN KEY(Event_id) REFERENCES Event(Event_id)
 );
 
 CREATE TABLE Hosts (
   Event_id INT(9) NOT NULL,
-  Game_name VARCHAR NOT NULL,
+  Game_name VARCHAR(255) NOT NULL,
   PRIMARY KEY(Event_id, Game_name),
   FOREIGN KEY(Event_id) REFERENCES Event(Event_id),
   FOREIGN KEY(Game_name) REFERENCES Game(Name)
@@ -69,21 +69,21 @@ CREATE TABLE Helpers (
 
 CREATE TABLE Stream_operators (
   Staff_id INT(9) NOT NULL,
-  Program_knowledge VARCHAR,
+  Program_knowledge VARCHAR(255),
   Bring_equipment BOOLEAN NOT NULL,
   PRIMARY KEY(Staff_id),
   FOREIGN KEY(Staff_id) REFERENCES Event_staff(Staff_id)
 );
 
 CREATE TABLE Controllers (
-  Controller_name VARCHAR,
-  Game_name VARCHAR NOT NULL,
+  Controller_name VARCHAR(255),
+  Game_name VARCHAR(255) NOT NULL,
   PRIMARY KEY(Controller_name, Game_name),
   FOREIGN KEY(Game_name) REFERENCES Game(Name)
 );
 
 CREATE TABLE Main_characters (
-  Character_name VARCHAR NOT NULL,
+  Character_name VARCHAR(255) NOT NULL,
   Username VARCHAR(20) NOT NULL,
   PRIMARY KEY(Character_name, Username),
   FOREIGN KEY(Username) REFERENCES Player(Username)

4213d99a84da5c4febcdae798f5e1f5e4d8bd8ac
Reno DuBois <rdubois96@gmail.com>
2016-05-01 04:22:31 +0000

    Fixed spacing/tab issues in authentication

--- authentication.py
+++ authentication.py
@@ -8,46 +8,36 @@ import mysql.connector
 	which is only set when a user logs in successfully.
 '''
 def requiresLogin(func):
-	@functools.wraps(func)
-	def wrapper(*args, **kwargs):
-	    if not request.get_cookie('logged_in_as'):
-		    redirect('/login/')
-		else:
-		    return func(*args, **kwargs)
-	return wrapper
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if not request.get_cookie('logged_in_as'):
+            redirect('/login/')
+        else:
+            return func(*args, **kwargs)
+    return wrapper
 
 
 def validateForm(form):
-	# Try and connect to the database
-	try:
-	    conn = mysql.connector.connect(user=MY_SQL_CONNECTION[0],
-	    password=MY_SQL_CONNECTION[1], host=MY_SQL_CONNECTION[2],
-	    database=MY_SQL_CONNECTION[3])
-	except mysql.connector.Error as err:
-	    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-		    print('Something is wrong w/ username and password')
-	    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-		    print('Test database doesn\'t exist')
+    # Try and connect to the database
+    try:
+        conn = mysql.connector.connect(user=MY_SQL_CONNECTION[0],
+        password=MY_SQL_CONNECTION[1], host=MY_SQL_CONNECTION[2],
+        database=MY_SQL_CONNECTION[3])
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Something is wrong w/ username and password')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('Test database doesn\'t exist')
         else:
             print(err)
-	else:
-	    userLength = len(form['username'])
-	    passLength = len(form['password'])
-	    if not form['username']:
-		    # Error; username can't be blank
-		    print('poo')
-	    elif not form['password']:
-		    # Error; password can't be blank
-		    print('more poo')
-	    # Sanitize input, then check database for match.
-	    else:
-            cursor = conn.cursor()
-            # Encrypt the entered password
-            password = hashlib.sha256(password.encode())
-            # Check to see if the username and password match.
-            query = "SELECT * FROM users WHERE username ='{}' and password ='{}'".format(username, password.hexdigest())
-            cursor.execute(query)
-            for r in cursor:
-		        print(r)
-            cursor.close()
-            conn.close()
+    else:
+        cursor = conn.cursor()
+        # Encrypt the entered password
+        password = hashlib.sha256(password.encode())
+        # Check to see if the username and password match.
+        query = "SELECT * FROM Player WHERE Username ='{}' and Password ='{}'".format(username, password.hexdigest())
+        cursor.execute(query)
+        for r in cursor:
+            print(r)
+        cursor.close()
+        conn.close()

5eb9be1ac6e1065158001c5174b989444ee67a65
Michael Beaver <mbbh8@mst.edu>
2016-05-04 20:34:08 -0500

    Commented out type checking

--- signup.py
+++ signup.py
@@ -12,28 +12,28 @@ def formValidation(form):
     password_confirm = form['password-confirm']
     #Create list to hold all the errors
     error = []
-    if type(username) is str:
-        if len(username) < 4 or len(username) > 20:
-            error.append('Username must be between 4 and 20 characters!')
-    else:
-        error.append('Please input a proper username')
-    if type(fname) is str:
-        if fname == '':
-            error.append('First name field must be filled out!')
-        if len(fname) > 30:
+    #if type(username) is str:
+    if len(username) < 4 or len(username) > 20:
+        error.append('Username must be between 4 and 20 characters!')
+    #else:
+    #    error.append('Please input a proper username')
+    #if type(fname) is str:
+    if fname == '':
+        error.append('First name field must be filled out!')
+    if len(fname) > 30:
         error.append('Names must be less than 30 characters!')
-    else:
-        error.append('Please input a proper first name')
-    if type(lname) is str:
-        if lname == '':
-            error.append('Last name field must be filled out!')
-        if len(lname) > 30:
-            error.append('Names must be less than 30 characters!')
-    else:
-        error.append('Please input a proper last name')
-    if type(location) is str:
-        if location == '':
-            error.append('Location field must be filled out!')
+    #else:
+    #    error.append('Please input a proper first name')
+    #if type(lname) is str:
+    if lname == '':
+        error.append('Last name field must be filled out!')
+    if len(lname) > 30:
+        error.append('Names must be less than 30 characters!')
+    #else:
+    #    error.append('Please input a proper last name')
+    #if type(location) is str:
+    if location == '':
+        error.append('Location field must be filled out!')
     if len(password) < 6 or len(password) > 36:
         error.append('Password must be between 6 and 36 characters!')
     if password != password_confirm:

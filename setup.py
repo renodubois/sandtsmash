8fb669add6f7e68c5b49a2ed06b57ba141e3b197
Michael Beaver <mbbh8@mst.edu>
2016-05-04 23:39:07 -0500

    Setting up list of characters

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
 # User, password, host, database
 MY_SQL_CONNECTION = ('root', 'dankmemes', '127.0.0.1', 'RollaSmash')
+
+meleeCharacters = []
+meleeFile = open("MeleeCharacters.txt", r)
+for line in meleeFile:
+    meleeCharacters.append(meleeFile.readline())
+
+for character in meleeCharacters
+    print(character)

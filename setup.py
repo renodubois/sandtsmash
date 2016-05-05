b5334c3a5b52ff2150d338202851f5592d99d0f7
Michael Beaver <mbbh8@mst.edu>
2016-05-04 23:48:20 -0500

    syntax errors

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # User, password, host, database
 MY_SQL_CONNECTION = ('root', 'dankmemes', '127.0.0.1', 'RollaSmash')
 
-with open("MeleeCharacters.txt") as meleeFile
+with open("MeleeCharacters.txt") as meleeFile:
     meleeCharacters = meleeFile.read()
 
 for character in meleeCharacters

f498b8627ac162c80f2e946079be8147eae70f51
Reno DuBois <rdubois96@gmail.com>
2016-05-05 01:45:19 +0000

    Lots of work on events

--- events.py
+++ events.py
@@ -34,18 +34,40 @@ def getCurrentEvents():
         cursor.execute("SELECT * FROM Event")
 
         for r in cursor:
-            eventTime = datetime.datetime.strptime(r[1])
+            eventTime = r[1]  #datetime.datetime.strptime(r[1], dateFormat)
             # Only grab events that have not happened yet.
             if eventTime > curTime:
                 eventInfo['id'] = r[0]
-                eventInfo['event-date'] = datetime.datetime.strptime(r[1])
+                eventInfo['eventDate'] = r[1].strftime("%B, %d %Y")  # datetime.datetime.strptime(r[1], dateFormat)
+                eventInfo['eventTime'] = r[1].strftime("%I:%M %p")
                 eventInfo['entryfee'] = float(r[2])
-                eventInfo['max-players'] = r[3]
+                eventInfo['maxPlayers'] = r[3]
                 eventInfo['location'] = r[4]
                 eventInfo['name'] = r[7]
+                eventInfo['attendees'] = findCompetitors(r[0])
+                eventInfo['numAttendees'] = len(findCompetitors(r[0]))
                 collectedEvents.append(eventInfo)
         return collectedEvents
 
+def findCompetitors(eventId):
+    try:
+        conn = mysql.connector.connect(user=MY_SQL_CONNECTION[0],
+        password=MY_SQL_CONNECTION[1], host=MY_SQL_CONNECTION[2],
+        database=MY_SQL_CONNECTION[3])
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Something is wrong w/ username and password')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('Test database doesn\'t exist')
+        else:
+            print(err) 
+    else:
+        cursor = conn.cursor()
+        attendees = []
+        cursor.execute("SELECT Username FROM Competes_in WHERE Event_id = '{}'".format(eventId))
+        for r in cursor:
+            attendees.append(r)
+        return attendees
 
 def getFinishedEvents():
     pass
